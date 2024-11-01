895bef9b9ce49cf75859c927e4fa2374865aa6cb
Anton-Deriabin <toh309@yandex.ru>
2024-11-01 14:58:25 +0300

    Добавлено логирование

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.exception.DuplicatedDataException;
@@ -12,6 +13,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/films")
 public class FilmController {
@@ -20,6 +22,7 @@ public class FilmController {
 
     @GetMapping
     public Collection<Film> findAll() {
+        log.info("Коллекция фильмов отправлена по запросу");
         return films.values();
     }
 
@@ -27,64 +30,79 @@ public class FilmController {
     public Film create(@Valid @RequestBody Film film) {
         for (Film value : films.values()) {
             if (film.getName().equals(value.getName())) {
+                log.error("Попытка занять уже используемое имя при добавлении");
                 throw new DuplicatedDataException(messageNameDuplicate);
             }
         }
         if (film.getDescription().length() > 200) {
+            log.error("Описание длиннее 200 символов при добавлении");
             throw new ValidationException("Описание не может быть длиннее 200 символов");
         }
         if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.error("Дата выхода раньше 28.12.1895 при добавлении");
             throw new ValidationException("Дата выхода не может быть раньше " +
                     "даты выхода первого в истории фильма");
         }
         if (film.getDuration() <= 0) {
+            log.error("Длительность фильма меньше или равна нулю при добавлении");
             throw new ValidationException("Длительность фильма должна быть больше нуля");
         }
         film.setId(getNextId());
+        log.debug("Фильму \"{}\" назначен id = {}", film.getName(), film.getId());
         films.put(film.getId(), film);
+        log.info("Фильм с id = {}  - добавлен", film.getId());
         return film;
     }
 
     @PutMapping
     public Film update(@Valid @RequestBody Film newFilm) {
         if (newFilm.getId() == null) {
+            log.error("Id фильма для обновления не указан");
             throw new ValidationException("Id должен быть указан");
-        }
-        for (Film value : films.values()) {
-            if (newFilm.getName().equals(value.getName())) {
-                throw new DuplicatedDataException(messageNameDuplicate);
-            }
+
         }
         if (newFilm.getDescription().length() > 200) {
+            log.error("Описание к обновлению длиннее 200 символов");
             throw new ValidationException("Описание не может быть длиннее 200 символов");
         }
         if (newFilm.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.error("Дата выхода к обновлению раньше 28.12.1895");
             throw new ValidationException("Дата выхода не может быть раньше " +
                     "даты выхода первого в истории фильма");
         }
         if (newFilm.getDuration() <= 0) {
+            log.error("Длительность к обновлению меньше или равна нулю");
             throw new ValidationException("Длительность фильма должна быть больше нуля");
         }
         if (films.containsKey(newFilm.getId())) {
             Film oldFilm = films.get(newFilm.getId());
+            log.trace("Создали переменную старого фильма для сравнения с новой");
             if (newFilm.getName() != null) {
                 for (Film value : films.values()) {
                     if (newFilm.getName().equals(value.getName())) {
+                        log.error("Попытка занять уже используемое имя при обновлении");
                         throw new DuplicatedDataException(messageNameDuplicate);
                     }
                 }
             }
             oldFilm.setName(newFilm.getName());
+            log.debug("Фильму с id = {} установлено имя - {}", newFilm.getId(), newFilm.getName());
             oldFilm.setDescription(newFilm.getDescription());
+            log.debug("Фильму с id = {} установлено описание - {}", newFilm.getId(), newFilm.getDescription());
             oldFilm.setReleaseDate(newFilm.getReleaseDate());
+            log.debug("Фильму с id = {} установлена дата выхода - {}", newFilm.getId(), newFilm.getReleaseDate());
             oldFilm.setDuration(newFilm.getDuration());
+            log.debug("Фильму с id = {} установлена длительность - {}", newFilm.getId(), newFilm.getDuration());
+            log.info("Фильм \"{}\" с id = {}  - обновлен", newFilm.getName(), newFilm.getId());
             return oldFilm;
         }
+        log.error("Попытка получить фильм с несуществующим id = {}", newFilm.getId());
         throw new NotFoundException(String.format("Фильм с id = %d  - не найден", newFilm.getId()));
     }
 
     private long getNextId() {
         long currentMaxId = films.keySet().stream().mapToLong(id -> id).max().orElse(0);
+        log.debug("Cоздали новый id = {} ", currentMaxId);
         return ++currentMaxId;
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.exception.DuplicatedDataException;
@@ -12,6 +13,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -20,75 +22,84 @@ public class UserController {
 
     @GetMapping
     public Collection<User> findAll() {
+        log.info("Коллекция пользователей отправлена по запросу");
         return users.values();
     }
 
     @PostMapping
     public User create(@Valid @RequestBody User user) {
-        if (!user.getEmail().contains("@")) {
-            throw new ValidationException("Email должен быть указан и должен содержать символ \"@\"");
-        }
         for (User value : users.values()) {
             if (user.getEmail().equals(value.getEmail())) {
+                log.error("Попытка занять уже используемый email при добавлении");
                 throw new DuplicatedDataException(messageEmailDuplicate);
             }
         }
         if (user.getLogin().contains(" ")) {
+            log.error("Логин содержит пробелы при добавлении");
             throw new ValidationException("Логин должен быть указан и не должен содержать пробелы");
         }
         if (user.getName() == null || user.getName().isBlank()) {
             user.setName(user.getLogin());
+            log.debug("Вместо имени использован логин при добавлении");
         }
         if (user.getBirthday().isAfter(LocalDate.now())) {
+            log.error("Дата рождения позднее текущей даты при добавлении");
             throw new ValidationException("Дата рождения не может быть позднее текущей даты");
         }
         user.setId(getNextId());
+        log.debug("Пользователю \"{}\" назначен id = {}", user.getName(), user.getId());
         users.put(user.getId(), user);
+        log.info("Пользователь с id = {}  - добавлен", user.getId());
         return user;
     }
 
     @PutMapping
     public User update(@Valid @RequestBody User newUser) {
         if (newUser.getId() == null) {
+            log.error("Id пользователя для обновления не указан");
             throw new ValidationException("Id должен быть указан");
         }
-        if (!newUser.getEmail().contains("@")) {
-            throw new ValidationException("Email должен быть указан и должен содержать символ \"@\"");
-        }
-        for (User value : users.values()) {
-            if (newUser.getEmail().equals(value.getEmail())) {
-                throw new DuplicatedDataException(messageEmailDuplicate);
-            }
-        }
         if (newUser.getLogin().contains(" ")) {
+            log.error("Логин к обновлению содержит пробелы");
             throw new ValidationException("Логин должен быть указан и не должен содержать пробелы");
         }
         if (newUser.getName() == null || newUser.getName().isBlank()) {
+            log.debug("Вместо имени использован логин при обновлении");
             newUser.setName(newUser.getLogin());
         }
         if (newUser.getBirthday().isAfter(LocalDate.now())) {
+            log.error("Дата рождения позднее текущей даты при обновлении");
             throw new ValidationException("Дата рождения не может быть позднее текущей даты");
         }
         if (users.containsKey(newUser.getId())) {
             User oldUser = users.get(newUser.getId());
+            log.trace("Создали переменную старого пользователя для сравнения с новым");
             if (newUser.getEmail() != null) {
                 for (User value : users.values()) {
                     if (newUser.getEmail().equals(value.getEmail())) {
+                        log.error("Попытка занять уже используемый email при обновлении");
                         throw new DuplicatedDataException(messageEmailDuplicate);
                     }
                 }
             }
             oldUser.setEmail(newUser.getEmail());
+            log.debug("Пользователю с id = {} установлен email - {}", newUser.getId(), newUser.getEmail());
             oldUser.setName(newUser.getName());
+            log.debug("Пользователю с id = {} установлено имя - {}", newUser.getId(), newUser.getName());
             oldUser.setLogin(newUser.getLogin());
+            log.debug("Пользователю с id = {} установлен логин - {}", newUser.getId(), newUser.getLogin());
             oldUser.setBirthday(newUser.getBirthday());
+            log.debug("Пользователю с id = {} установлена дата рождения - {}", newUser.getId(), newUser.getBirthday());
+            log.info("Пользователь \"{}\" с id = {}  - обновлен", newUser.getName(), newUser.getId());
             return oldUser;
         }
+        log.error("Попытка обновить пользователя с несуществующим id = {}", newUser.getId());
         throw new NotFoundException(String.format("Пользователь с id = %d  - не найден", newUser.getId()));
     }
 
     private long getNextId() {
         long currentMaxId = users.keySet().stream().mapToLong(id -> id).max().orElse(0);
+        log.debug("Cоздали новый id = {} ", currentMaxId);
         return ++currentMaxId;
     }
 }
