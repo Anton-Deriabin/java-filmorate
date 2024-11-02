2b13ffb2aa7f8d1ca05f8408bec8f544c6c9aad3
Anton-Deriabin <toh309@yandex.ru>
2024-11-02 11:55:25 +0300

    Доступ к полям классов Film и User установлен через аннотацию.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -4,22 +4,25 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDate;
 
 @Data
 @EqualsAndHashCode(of = { "name" })
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Film {
-    private Long id;
+    Long id;
     @NotNull
     @NotBlank
-    private String name;
+    String name;
     @Size(max = 200, message = "Описание не может быть длиннее 200 символов")
-    private String description;
-    private LocalDate releaseDate;
+    String description;
+    LocalDate releaseDate;
     @NotNull
     @Positive(message = "Продолжительность должна быть положительным числом")
-    private Integer duration;
+    Integer duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,25 +1,28 @@
 package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.*;
+import lombok.AccessLevel;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDate;
 
 @Data
 @EqualsAndHashCode(of = { "email" })
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class User {
-    private Long id;
+    Long id;
     @NotNull
     @NotBlank
     @Email
-    private String email;
-    private String name;
+    String email;
+    String name;
     @NotNull
     @NotBlank
     @Pattern(regexp = "^[^\\s]+$", message = "Логин не должен содержать пробелы")
-    private String login;
+    String login;
     @NotNull
     @Past
-    private LocalDate birthday;
+    LocalDate birthday;
 }
