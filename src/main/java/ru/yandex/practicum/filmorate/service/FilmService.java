7fea48e9a6d3ad6a17bfa91c3f23e0cc0d8bb969
ScoobyWraith <161945036+ScoobyWraith@users.noreply.github.com>
2025-01-16 10:50:24 +0300

    add-recommendations (#24)
    * [Рекомендации] Реализация функциональности "Рекомендации"
    
    ---------
    
    Co-authored-by: m.kiselev <sanwiraight@gmail.com>

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +25,14 @@ public class FilmService {
 
     public List<FilmDto> findAll() {
         List<Film> films = filmRepository.findAll();
+        filmEnrichmentService.enrichFilms(films);
+        return films.stream()
+                .map(FilmMapper::mapToFilmDto)
+                .toList();
+    }
+
+    public List<FilmDto> findAllWithIds(Set<Long> filmIds) {
+        List<Film> films = filmRepository.findAllWithIds(filmIds);
         filmEnrichmentService.enrichFilms(films);
         return films.stream()
                 .map(FilmMapper::mapToFilmDto)
--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,24 +1,37 @@
 package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.dto.FilmDto;
 import ru.yandex.practicum.filmorate.dto.UserDto;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.mapper.UserMapper;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.FriendshipRepository;
+import ru.yandex.practicum.filmorate.storage.LikeRepository;
 import ru.yandex.practicum.filmorate.storage.UserRepository;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
     private final FriendshipRepository friendshipRepository;
+    private final LikeRepository likeRepository;
+    private final FilmService filmService;
 
-    public UserService(UserRepository userRepository, FriendshipRepository friendshipRepository) {
+    public UserService(UserRepository userRepository,
+                       FriendshipRepository friendshipRepository,
+                       LikeRepository likeRepository,
+                       FilmService filmService) {
         this.userRepository = userRepository;
-        this.friendshipRepository =  friendshipRepository;
+        this.friendshipRepository = friendshipRepository;
+        this.likeRepository = likeRepository;
+        this.filmService = filmService;
     }
 
     public List<UserDto> findAll() {
@@ -46,9 +59,8 @@ public class UserService {
         if (newUser.getId() == null) {
             throw new ValidationException("Id должен быть указан");
         }
-        if (findById(newUser.getId()) == null) {
-            throw new NotFoundException(String.format("Пользователь с id=%d не найден", newUser.getId()));
-        }
+
+        checkUserExists(newUser.getId());
         checkName(newUser);
         return UserMapper.mapToUserDto(userRepository.update(newUser));
     }
@@ -58,9 +70,8 @@ public class UserService {
     }
 
     public List<UserDto> getFriends(Long receiver) {
-        if (userRepository.findById(receiver).isEmpty()) {
-            throw new NotFoundException(String.format("Пользователь с id=%d не найден", receiver));
-        }
+        checkUserExists(receiver);
+
         return userRepository.getFriends(receiver)
                 .stream()
                 .map(UserMapper::mapToUserDto)
@@ -80,6 +91,54 @@ public class UserService {
                 .stream()
                 .map(UserMapper::mapToUserDto)
                 .toList();
+    }
+
+    public List<FilmDto> getRecommendationsForUser(Long userId) {
+        checkUserExists(userId);
+
+        List<Long> filmsLikedByUser = likeRepository.getFilmIdsLikedByUser(userId);
+
+        if (filmsLikedByUser.isEmpty()) {
+            return List.of();
+        }
+
+        List<Long> usersWithSameTaste = likeRepository.getUserIdsWhoLikedFilms(filmsLikedByUser, userId);
+
+        if (usersWithSameTaste.isEmpty()) {
+            return List.of();
+        }
+
+        Map<Long, Set<Long>> userToLikedFilmsMap = likeRepository.getUsersWithLikedFilms(usersWithSameTaste);
+
+        long nearestUserIdByTaste = userToLikedFilmsMap.keySet().stream()
+                // для каждого ИД юзера вычислить число общих фильмов с исследуемым юзером
+                .map((Long id) -> {
+                    List<Long> commonFilmsWithLikes = new ArrayList<>(filmsLikedByUser);
+                    commonFilmsWithLikes.retainAll(userToLikedFilmsMap.get(id));
+                    return Map.of(
+                            "userId", id,
+                            "commonFilms", (long) commonFilmsWithLikes.size()
+                    );
+                })
+                // отбор наибольшего совпадения
+                .max(Comparator.comparingLong(m -> m.get("commonFilms")))
+                .orElse(Map.of("userId", -1L))
+                .get("userId");
+
+        if (nearestUserIdByTaste < 0) {
+            return List.of();
+        }
+
+        Set<Long> recommendations = userToLikedFilmsMap.get(nearestUserIdByTaste);
+        // удалить из recommendations все фильмы, которые уже лайкал исследуемый юзер
+        filmsLikedByUser.forEach(recommendations::remove);
+        return filmService.findAllWithIds(recommendations);
+    }
+
+    private void checkUserExists(long userId) {
+        if (userRepository.findById(userId).isEmpty()) {
+            throw new NotFoundException(String.format("Пользователь с id=%d не найден", userId));
+        }
     }
 
     private void checkName(User user) {
